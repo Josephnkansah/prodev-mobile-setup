986d7e99cfc1620a169e00a11803dfcf2a1c8105
Joseph nkansah <133186186+Josephnkansah@users.noreply.github.com>
2025-07-12 12:13:47 -1200

    Delete prodev-mobile-app-0x00 directory

--- prodev-mobile-app-0x00/app-example/constants/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from 'react-native';
-
-export default function TabHome() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>** First App Created**</Text>
-    </View>
-  );
-}
